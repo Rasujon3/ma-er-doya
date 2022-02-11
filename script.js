1b233f922e207b76d18e6d34be1755503c894134
Rasujon3 <rasujon3@gmail.com>
2022-02-12 00:11:41 +0600

    my-soluation with function

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 // function
-function inputAdd() {}
+function inputAdd(dValue, dOutput, bOutput) {
+  let inputValueText = document.getElementById(dValue);
+  let outputText = document.getElementById(dOutput);
+  let totalBalance = document.getElementById(bOutput);
+
+  if (
+    dValue == "deposit-value" &&
+    dOutput == "deposit-output" &&
+    bOutput == "balance-output"
+  ) {
+    outputText.innerText =
+      Number(outputText.innerText) + Number(inputValueText.value);
+
+    totalBalance.innerText =
+      Number(totalBalance.innerText) + Number(inputValueText.value);
+  } else {
+    if (Number(totalBalance.innerText) >= Number(outputText.innerText)) {
+      outputText.innerText =
+        Number(outputText.innerText) + Number(inputValueText.value);
+      totalBalance.innerText =
+        Number(totalBalance.innerText) - Number(inputValueText.value);
+    } else {
+      alert("Unsufficient Balance");
+    }
+  }
+
+  // clear input
+  inputValueText.value = "";
+}
 
 // deposit
 function deposit() {
-  let depositValue = document.getElementById("deposit-value");
+  /* let depositValue = document.getElementById("deposit-value");
   let depositOutput = document.getElementById("deposit-output");
   let balanceOutput = document.getElementById("balance-output");
 
@@ -14,24 +42,26 @@ function deposit() {
     Number(balanceOutput.innerText) + Number(depositValue.value);
 
   // clear input
-  depositValue.value = "";
+  depositValue.value = ""; */
+  inputAdd("deposit-value", "deposit-output", "balance-output");
 }
 
 // withdraw
 function withdraw() {
-  let withdrawValue = document.getElementById("withdraw-value");
-  let withdrawOutput = document.getElementById("withdraw-output");
-  let balanceOutput = document.getElementById("balance-output");
+  // let withdrawValue = document.getElementById("withdraw-value");
+  // let withdrawOutput = document.getElementById("withdraw-output");
+  // let balanceOutput = document.getElementById("balance-output");
 
-  if (Number(balanceOutput.innerText) > Number(withdrawOutput.innerText)) {
-    withdrawOutput.innerText =
-      Number(withdrawOutput.innerText) + Number(withdrawValue.value);
-    balanceOutput.innerText =
-      Number(balanceOutput.innerText) - Number(withdrawValue.value);
-  } else {
-    alert("Unsufficient Balance");
-  }
+  // if (Number(balanceOutput.innerText) > Number(withdrawOutput.innerText)) {
+  //   withdrawOutput.innerText =
+  //     Number(withdrawOutput.innerText) + Number(withdrawValue.value);
+  //   balanceOutput.innerText =
+  //     Number(balanceOutput.innerText) - Number(withdrawValue.value);
+  // } else {
+  //   alert("Unsufficient Balance");
+  // }
 
-  // clear input
-  withdrawValue.value = "";
+  // // clear input
+  // withdrawValue.value = "";
+  inputAdd("withdraw-value", "withdraw-output", "balance-output");
 }
